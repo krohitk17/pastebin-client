67227f558844796c50975a6de13fb14b22b1d6a2
darshan <darshpat01@gmail.com>
2023-02-26 23:24:19 +0530

    removed container from nav

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import NavItem from "./NavItem";
 function Navbar() {
   return (
     <div className="w-[100%] z-40 bg-opacity-10 backdrop-blur-sm bg-red-500">
-      <ul className="flex flex-row container mx-auto xl:py-1">
+      <ul className="flex flex-row mx-auto xl:py-1">
         <li className="p-1 m-1 text-2xl pr-4 ">PASTEBIN</li>
         <NavItem className="ml-auto">01</NavItem>
         <NavItem>02</NavItem>
